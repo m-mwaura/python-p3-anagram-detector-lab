32dc9e293be7591e35d6cae7479827bf5ca6cf75
felix mwangi <fmwauramwangi@gmail.com>
2025-02-18 17:36:40 +0300

    lib/anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+#import ipdb
+
+class Anagram:
+    #ipdb.set_trace()
+    def __init__(self, word):
+        self.word = word.lower()
+        
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+
+        matches = [word for word in word_list if sorted(word.lower()) == sorted_word]
+        
+        return matches
